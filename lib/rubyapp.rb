590ea9d406504eb72990d5ef625ee673e9d2153f
Alex <alexiseliudrdz@gmail.com>
2016-07-20 15:49:47 -0500

    added test of sending messages.

--- lib/rubyapp.rb
+++ lib/rubyapp.rb
@@ -1,6 +1,17 @@
 require "rubyapp/init"
 require "sinatra/base"
 
-class RubyApp < Sinatra::Base
-	
+class Rubyapp < Sinatra::Base
+	post "/messages" do
+		if params[:message].nil? ||
+		/\A\s*\Z/ =~ params[:message]
+			halt 400
+		end
+		Rubyapp.push(params[:message])
+		201		
+	end
+
+	def self.push(message)
+		#FIX: send a message
+	end
 end
--- test/test_rubyapp.rb
+++ test/test_rubyapp.rb
@@ -1,7 +1,41 @@
+require "rubyapp/init"
+
 require "minitest/autorun"
+require 'mocha/mini_test'
+require "rack/test"
+
 require "rubyapp"
+
 class TestRubyApp < MiniTest::Unit::TestCase
-	def test_sanity
-		flunk
+	include Rack::Test::Methods
+	
+	def setup
+		@app = nil
 	end
+
+	def app
+		@app ||= Rubyapp.new
+	end
+	def test_post_message
+		app.expects(:push).with("Hello!")
+		post("/messages", meesage: "Hello!")
+		assert_equal(201,response.status)
+	end
+
+
+	def test_post_message_missing
+		app.expects(:push).never
+		post("/messages")
+		assert_equal(400,response.status)
+	end
+
+	def test_post_message_empty
+		app.expects(:push).never
+		post("/messages", message: "  ")
+		assert_equal(400,response.status)
+	end
+	
+
+	alias_method :request, :last_request
+	alias_method :response, :last_response
 end
