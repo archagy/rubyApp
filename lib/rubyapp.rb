cb3861f94b89b833fcf548abeeaa827655bdfce2
Alex <alexiseliudrdz@gmail.com>
2016-07-21 17:17:00 -0500

    test and display message in console when button go is pushed

--- lib/rubyapp.rb
+++ lib/rubyapp.rb
@@ -9,6 +9,7 @@ Pusher.app_id = '228182',
 Pusher.key = '34b7830c76670bb6edee',
 Pusher.secret = '4fbe6e58bfae108d637f'
 
+
 class Rubyapp < Sinatra::Base
 	set :root, File.expand_path("../..", __FILE__)
 
@@ -21,13 +22,16 @@ class Rubyapp < Sinatra::Base
     	if params[:message].nil? ||
         	/\A\s*\Z/ =~ params[:message]
       		halt 400
+      	else
+	    	Rubyapp.push(params[:message])
+	    	204
     	end
-    	Rubyapp.push(params[:message])
-    	204
  	 end
 
 
-	def self.push(message)
-		#FIX: send a message
+	def self.push message
+		p Pusher
+		pusher.trigger('messages', 'message', {:message => message})
+
 	end
 end
--- test/test_rubyapp.rb
+++ test/test_rubyapp.rb
@@ -1,36 +1,52 @@
 require "rubyapp/init"
 require "minitest/autorun"
-require 'mocha/mini_test'
 require "rack/test"
 require "rubyapp"
+
+
+
+class Pusher::Channel
+	
+	def self.triggers
+		@triggers ||= []
+
+	end 
+	def trigger! event, data, *args
+		self.class.triggers << [event, data]
+	end
+end
 
 class TestRubyApp < MiniTest::Unit::TestCase
 	include Rack::Test::Methods
 	
 	def setup
-		@app = nil
+		Pusher::Channel.triggers.clear 
 	end
 	def app
 		Rubyapp.new
 	end
 	def test_post_message
-		app.expects(:push).with("Hello!")
-		post("/messages", meesage: "Hello!")
-		assert_equal(204,response.status)
+		post "/messages", message: "Hello!"
+		puts response.status
+		assert_equal 204, response.status
+		assert_equal ["message", "Hello!"],
+		 Pusher::Channel.triggers.first
 	end
 
-
-	def test_post_message_missing
-		app.expects(:push).never
-		post("/messages")
-		assert_equal(400,response.status)
-	end
 
 	def test_post_message_empty
-		app.expects(:push).never
 		post("/messages", message: "  ")
 		assert_equal(400,response.status)
+		assert Pusher::Channel.triggers.empty?
+
 	end
+
+	def test_post_message_missing
+		post("/messages")
+		assert_equal(400,response.status)
+		assert Pusher::Channel.triggers.empty?
+	end
+
 	
 
 	alias_method :request, :last_request
