58c040cc7d033062463d11e432c90987594380cb
Alex <alexiseliudrdz@gmail.com>
2016-07-21 16:02:19 -0500

    Launch push notification

--- lib/rubyapp.rb
+++ lib/rubyapp.rb
@@ -3,21 +3,17 @@ require "sinatra/base"
 require "pusher"
 
 
-unless "production" == ENV["RACK_ENV"]
-	pusher_client = Pusher::Client.new(
-  		app_id: '228182',
-  		key: '34b7830c76670bb6edee',
-  		secret: '4fbe6e58bfae108d637f'
-	);	
-end
 
+
+Pusher.app_id = '228182',
+Pusher.key = '34b7830c76670bb6edee',
+Pusher.secret = '4fbe6e58bfae108d637f'
 
 class Rubyapp < Sinatra::Base
 	set :root, File.expand_path("../..", __FILE__)
 
 	get '/' do
-		[200, {"content-type" => "text/html"}, 
-			File.read("public/index.html")]
+		erb :index
 	end
 
 
